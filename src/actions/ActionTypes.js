02a6566ccea79ff0cee2e2602e67fd452dd36bc1
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-09-18 10:17:15 -0700

    add index.js reducer file, update ActionTypes.js

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,3 +1,5 @@
 import { bindActionCreators } from "redux";
 
-export const KEG = 'ADD_KEG';
+export const ADD_KEG = 'ADD_KEG';
+export const DELETE_KEG = 'DELETE_KEG';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
